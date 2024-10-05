cd2f3ee3a2ce5e66c4173ef0a6a8485508d4d395
Marcel Hellkamp <marc@gsites.de>
2024-10-05 23:03:04 +0200

    Missing dependency

--- prepare_venv.sh
+++ prepare_venv.sh
@@ -3,13 +3,14 @@ python3 -mvenv venv
 . venv/bin/activate
 
 pip install -U pip tabulate matplotlib numpy
+pip install streaming-form-data
 pip install werkzeug
+pip install python-multipart
 
 # The "python-multipart" package installs itself as just "multipart", causing a
 # name conflict with multipart.py. For this benchmark we can simply rename it,
 # but for actual applications this may be a real issue if some dependency pulls
 # in "python-multipart" and breaks "multipart".
-pip install python-multipart
 pushd venv/lib/python3.*/site-packages/
   mv multipart python_multipart
   mv multipart.dist-info python_multipart.dist-info
